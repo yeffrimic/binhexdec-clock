55160da3835d647405bbae6d00ce35895b14e07e
Yeffri J. Salazar <yeffrisalazar1@gmail.com>
2021-11-02 23:52:16 -0600

    Update binhexdec-clock.ino

--- binhexdec-clock/binhexdec-clock.ino
+++ binhexdec-clock/binhexdec-clock.ino
@@ -5,29 +5,41 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
-#define boton1 D7
-#define boton2 D6
-#define boton3 D5
-#define boton4 D4
+#define BUTTON_1 D7
+#define BUTTON_2 D6
+#define BUTTON_3 D4
+#define BUTTON_4 D5
+#define MATRIX_PIN D3
 
-#define PIN D3
-boolean HourOrMinutesIs = true;
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, MATRIX_PIN,
+                            NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
+                            NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+                            NEO_GRB            + NEO_KHZ800);
+
+
+
+boolean isHour = true;
 WiFiUDP ntpUDP;
 
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", -6 * 3600, 60000);
 
-const char *ssid     = "Xibalba Hackerspace";
-const char *password = "holi1234";
+const char *ssid     = "holii";
+const char *password = "estalibre";
 
 int hours = 0;
 int minutes = 0;
 int seconds = 0 ;
 int newHours = 0;
 int newMinutes = 0;
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
-                            NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
-                            NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-                            NEO_GRB            + NEO_KHZ800);
+
+char hourHex[4];
+char minuteHex[4];
+char secondsHex[4];
+
+int x = matrix.width();
+int r = 0;
+int pass = 0;
 
 
 const uint16_t colors[] = {
@@ -42,32 +54,80 @@ void decToBin(unsigned int in, int pos) {
     state = in & (1 << i);
     //Serial.println(state);
     if (state) {
-      matrix.setPixelColor(pos + i, colors[1]);
-    } else {
       matrix.setPixelColor(pos + i, colors[0]);
+    } else {
+      matrix.setPixelColor(pos + i, 0);
     }
   }
   // Serial.println();
 }
 
-int button(int pin, int x, boolean y) {
-  boolean estado = digitalRead(pin);
-  Serial.println(x);
+int button(int MATRIX_PIN, int x, boolean y) {
+  boolean estado = digitalRead(MATRIX_PIN);
+  //Serial.println(x);
   if (estado == LOW) {
-    delay(10);
-    Serial.println(estado);
+    delay(30);
+    //Serial.println(estado);
     if (y)  x++;
     else x--;
   }
-  Serial.println(x);
+  // Serial.println(x);
   return x;
 }
 
+void updateTime() {
+
+  hours = timeClient.getHours() + newHours;
+  minutes = timeClient.getMinutes() + newMinutes;
+  seconds = timeClient.getSeconds();
+}
+
+void showDisplay() {
+
+  switch (r) {
+    case 0:
+      matrix.setTextColor(colors[1]);
+      matrix.fillScreen(0);
+      matrix.setCursor(x, 0);
+      matrix.print(F(""));
+      matrix.print(hours);
+      matrix.print(":");
+      matrix.print(minutes);
+      matrix.print(":");
+      matrix.print(seconds);
+      break;
+    case 1:
+      matrix.setTextColor(colors[0]);
+      matrix.fillScreen(0);
+      matrix.setCursor(x, 0);
+      matrix.print(F(""));
+      matrix.print(hourHex);
+      matrix.print(":");
+      matrix.print(minuteHex);
+      matrix.print(":");
+      matrix.print(secondsHex);
+      break;
+    case 2:
+      matrix.fillScreen(0);
+      decToBin(hours, 128);
+      decToBin(minutes, 144);
+      decToBin(seconds, 160);
+      break;
+  }
+
+
+  if (--x < -46) {
+    x = matrix.width();
+
+  }
+  matrix.show();
+}
+
 void setup() {
-  pinMode(boton1, INPUT_PULLUP);
-  pinMode(boton2, INPUT_PULLUP);
-  pinMode(boton3, INPUT_PULLUP);
-  pinMode(boton4, INPUT_PULLUP);
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  pinMode(BUTTON_3, INPUT_PULLUP);
+  pinMode(BUTTON_4, INPUT_PULLUP);
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(10);
@@ -87,108 +147,33 @@ void setup() {
 
 
 
-int x    = matrix.width();
-int r = 0;
-int pass = 0;
 
 void loop() {
-
-  if (hours > 23) {
-    hours = 0;
-    newHours = 0;
+  updateTime();
+  if (digitalRead(BUTTON_4) == LOW) {
+    delay(30);
+    isHour = !isHour;
   }
-  if ( hours < 0) {
-    hours = 23;
-    newHours = 0;
+  if (isHour) {
+    newHours = button(BUTTON_1, newHours, true);
+    newHours = button(BUTTON_2, newHours, false);
+  } else {
+    newMinutes = button(BUTTON_1, newMinutes, true);
+    newMinutes = button(BUTTON_2, newMinutes, false);
   }
-  if (minutes > 59) {
-    minutes = 0;
-    newMinutes = 0;
+  if ( digitalRead(BUTTON_3) == LOW) {
+    delay(30);
+    if (++r >= 3) {
+      r = 0;
+    }
   }
-  if (minutes < 0) {
-    
-    minutes = 59;
-    newMinutes = 0;
-  }
-    hours = timeClient.getHours() + newHours;
-    minutes = timeClient.getMinutes() + newMinutes;
-    seconds = timeClient.getSeconds();
-    if (digitalRead(boton4) == LOW) {
-      delay(10);
-      HourOrMinutesIs = !HourOrMinutesIs;
-    }
-    if (HourOrMinutesIs) {
-      newHours = button(boton1, newHours, true);
-      newHours = button(boton2, newHours, false);
-    } else {
 
-      newMinutes = button(boton1, newMinutes, true);
-      newMinutes = button(boton2, newMinutes, false);
-    }
-    if ( digitalRead(boton3) == LOW) {
+  sprintf(hourHex, "%X", hours);
+  sprintf(minuteHex, "%X", minutes);
+  sprintf(secondsHex, "%X", seconds);
 
-      if (++r >= 3) {
-        r = 0;
-      }
-    }
+  showDisplay();
 
-    char hourHex[4];
-    char minuteHex[4];
-    char secondsHex[4];
-    sprintf(hourHex, "%X", hours);
-    sprintf(minuteHex, "%X", minutes);
-    sprintf(secondsHex, "%X", seconds);
-    //
-    Serial.println(hours);
-    /* Serial.print(":");
-      Serial.print(minutes);
-      Serial.print(":");
-      Serial.println(seconds);
-      Serial.print(hourHex);
-      Serial.print(":");
-      Serial.print(minuteHex);
-      Serial.print(":");
-      Serial.println(secondsHex);*/
-
-
-    switch (r) {
-      case 0:
-        matrix.fillScreen(0);
-        matrix.setCursor(x, 0);
-        matrix.print(F(""));
-        matrix.print(hours);
-        matrix.print(":");
-        matrix.print(minutes);
-        matrix.print(":");
-        matrix.print(seconds);
-        break;
-      case 1:
-        matrix.fillScreen(0);
-        matrix.setCursor(x, 0);
-        matrix.print(F(""));
-        matrix.print(hourHex);
-        matrix.print(":");
-        matrix.print(minuteHex);
-        matrix.print(":");
-        matrix.print(secondsHex);
-        break;
-      case 2:
-
-        decToBin(hours, 128);
-        decToBin(minutes, 144);
-        decToBin(seconds, 160);
-        break;
-
-    }
-
-
-    if (--x < -46) {
-      x = matrix.width();
-      matrix.setTextColor(colors[pass]);
-    }
-    matrix.show();
-    timeClient.update();
-    //Serial.println(timeClient.getFormattedTime());
-
-    delay(50);
-  }
+  timeClient.update();
+  delay(50);
+}
